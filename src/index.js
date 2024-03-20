99c6a1b17dec4619fa1746816cc117b2ea647fa2
Gaurav <gaurav.b21@iiits.in>
2024-03-21 02:14:05 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 //require("dotenv").config({ path: "./env" });
 
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: "./env"
+  path: "./env",
 });
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!!", err);
+  });
 
 /*
 import express from "express";
